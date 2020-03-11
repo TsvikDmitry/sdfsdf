a6b67ed41d977d8a969afbb7cacde07e27f897f6
Dima <dima@gmail.com>
2020-03-11 23:02:40 +0200

    первый комит

---
+++ lesson 1/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+a =5;
+console.log(a);
+/* var namber = 1;
+var string = "hi";
+var sym = Symbol();
+var boolean = true;
+
+//null;
+//undefined;
+var obj = {}; // свойства обьекта или метод
+
+
+console.log(4 / 0);
+console.log('string' * 9);
+console.log('adfsdf');
+let soms;
+console.log(soms);
+
+let persone = {
+  name: 'Jone',
+  age: 25,
+  isMarrid: false
+};
+
+console.log(persone["age"]);
+let arr = ['1', '2', '3'];
+let arr2 = [1, 2, 3, 4, 5];
+//alert("Hello word");
+/* let answer = confirm("are you age?");
+console.log(answer);
+ */
+//let myAge = +prompt("are you age?", "");
+//console.log(typeof (myAge));
+
+/* console.log(typeof (null));
+console.log(typeof (arr2));
+
+console.log("sds" + " - object");
+console.log(1+ + " - object");
+ */
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+
+console.log("2" === "2");
+
+let isChecked = false,
+  isClose = false;
+
+console.log(isChecked || isClose);
